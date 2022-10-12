cfbb50da687c4bf0e058e47ac43eef5508429f44
lsh0927 <lsheon0927@naver.com>
2022-10-12 16:43:12 +0900

    개미_군단

---
+++ algorithm_java/개미_군단.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        int a=0;
+        int b=0;
+        
+  
+        while(hp>4)
+        { 
+         hp-=5;
+         a++;
+        }
+        if(hp==4 || hp==2)
+        {
+            b=2;
+        }
+        else if(hp==3 ||hp==1)
+        {
+            b=1;
+        }
+        
+        answer=a+b;
+        
+        
+        return answer;
+    }
+}
