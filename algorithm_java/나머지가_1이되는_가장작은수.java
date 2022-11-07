b2ea79a6b9e93afdd7000e62f479d0a10b91dd9f
lsh0927 <lsheon0927@naver.com>
2022-11-07 19:49:01 +0900

    나머지가 1이되도록 하는 수

---
+++ algorithm_java/나머지가_1이되는_가장작은수.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int n) {
+ 
+        int count=1;
+        
+        while(n%count!=1)
+            count++;
+        
+        return count;
+    }
+} 나머지가_1이되는_가장작은수 {
+    
+}
