59fbf4de65866eef9bee6a7fcd8747c39a3d24fd
lsh0927 <lsheon0927@naver.com>
2022-11-20 16:46:51 +0900

    3진법_변환

---
+++ algorithm_java/3진법_변환.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int a=0;
+        int b=0;
+       while(n>=3^a)
+       {
+           a++;
+       }
+        
+        int answer = 0;
+        return answer;
+    }
+    
+    public int devide(int k)
+    {
+        while(k>3^)
+    }
+    
+}
+//45->3진법->27+18+0+0 -> 1200-> 앞뒤 반전->0021->10진수->7
+//3진법으로 나타내는 함수를 직접 구현하고 싶은데 왜 안대지 하
