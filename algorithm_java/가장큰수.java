746970b38409599bd3a06f1030fc13b8c3a36947
lsh0927 <lsheon0927@naver.com>
2023-02-08 11:04:01 +0900

    가장큰수

--- algorithm_java/가장큰수.java
+++ algorithm_java/가장큰수.java
@@ -1,34 +1,58 @@
+// import java.util.Arrays;
+// import java.util.Collections;
+
+// class Solution {
+//     public String solution(int[] numbers) {
+//         String answer = "";
+        
+//         Integer[] num = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        
+//         int []arr= new int[numbers.length];
+        
+//         for(int i=0;i<num.length;i++)
+//         {
+//             while(num[i]<10)
+//             {
+//                 int temp2=num[i];
+//                 num[i]/=10;
+//                 arr[i]=temp2%10; 
+//             }
+               
+//         }     
+        
+//         Arrays.sort(num,Collections.reverseOrder());
+        
+//          for(int i=0;i<num.length;i++)
+//         {
+//             System.out.println(num[i]);     
+//         }
+        
+//         return "";
+//         //배열은 int형이지만, 정답은 string형으로 바꾸어 제출해야한다 (int의 범위초과를 고려)
+     
+//     }
+// }
+
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        
-        Integer[] num = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-        
-        int []arr= new int[numbers.length];
-        
-        for(int i=0;i<num.length;i++)
-        {
-            while(num[i]<10)
-            {
-                int temp2=num[i];
-                num[i]/=10;
-                arr[i]=temp2%10; 
+        String[] nums = new String[numbers.length];
+
+        for (int i=0; i<nums.length; i++) 
+            nums[i] = numbers[i] + "";
+
+        Arrays.sort(nums, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
             }
-               
-        }     
-        
-        Arrays.sort(num,Collections.reverseOrder());
-        
-         for(int i=0;i<num.length;i++)
-        {
-            System.out.println(num[i]);     
-        }
-        
-        return "";
-        //배열은 int형이지만, 정답은 string형으로 바꾸어 제출해야한다 (int의 범위초과를 고려)
-     
+        });
+
+        String ans = "";
+        for (int i=0; i<numbers.length; i++)
+            ans += nums[i];
+
+        return ans.charAt(0) == '0' ? "0" : ans;
     }
 }
