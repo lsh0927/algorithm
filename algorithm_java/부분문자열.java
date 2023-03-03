f1e38ca6e41ab25c9521ad36e97f97f427d471bd
lsh0927 <lsheon0927@naver.com>
2023-03-03 16:41:40 +0900

    부분문자열

--- algorithm_java/부분문자열.java
+++ algorithm_java/부분문자열.java
@@ -4,8 +4,8 @@ class Solution
     {
         int answer = 0;
 
-        for(int i=0; i<=t.length()-p.length(); i++)
-            if(Long.parseLong(t.substring(i, i+p.length())) <= Long.parseLong(p))
+        for(int i=0; i<=t.length()-p.length(); i++) //문자열의 길이 
+            if(Long.parseLong(t.substring(i, i+p.length())) <= Long.parseLong(p)) //str-> long타입으로 변환 + substring: i번째 수 뒤부터~ 끝 인덱스 바로 전까지만 남김
                 answer++;
 
         return answer;
