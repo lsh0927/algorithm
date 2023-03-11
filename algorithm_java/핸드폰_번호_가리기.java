f33e11e17b41ea1652cc2aa8e1d2f5eff3b6ae70
lsh0927 <lsheon0927@naver.com>
2023-03-11 11:55:25 +0900

    StringBuffer

---
+++ algorithm_java/핸드폰_번호_가리기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        //같은 1단계 맞나 왜케 난이도 차이 심함?
+        //substring 0,length-4해서 다 *로 바꾸면 될듯
+        //ㅋㅋ 안댐
+        int len=phone_number.length();
+        StringBuffer sb = new StringBuffer(); //StringBuffer 를 이용하자!
+         sb.append(phone_number);
+        
+        for(int i=0;i<len-4;i++)
+         sb.replace(i,i+1,"*");
+        
+         answer = sb.toString(); //항상 타입을 신경 쓸것
+        return answer;
+        
+        //원래 였으면 배열로 split해서 접근했을텐데, 버퍼를 이용한다는 걸 알고나니 더 편한 느낌
+    }
+}
