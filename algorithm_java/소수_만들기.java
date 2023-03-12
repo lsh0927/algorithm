2bd30a57581e22b1fb0ce22353980b488564f9fb
lsh0927 <lsheon0927@naver.com>
2023-03-12 14:45:54 +0900

    소수만들기

---
+++ algorithm_java/소수_만들기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] nums) {
+        int ans = 0;
+
+        for(int i = 0; i < nums.length - 2; i ++){
+            for(int j = i + 1; j < nums.length - 1; j ++){
+                for(int k = j + 1; k < nums.length; k ++ ){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
+                        ans += 1;  
+                    } 
+                }
+            }
+        }
+        return ans;
+    }
+    public Boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i <= (int)Math.sqrt(num); i ++){
+            if(num % i == 0) cnt += 1; 
+        }
+        return cnt == 1;
+    }
+}
