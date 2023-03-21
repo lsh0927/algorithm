23aedf5cad9a37ad23372f0dd5f0163c89241684
lsh0927 <lsheon0927@naver.com>
2023-03-21 16:09:25 +0900

    삼항연산자

--- algorithm_java/Jed문자열_만들기.java
+++ algorithm_java/Jed문자열_만들기.java
@@ -1,17 +1,31 @@
+// class Solution{
+//     public String solution(String s) {
+//         String answer = "";
+        
+//         answer=s.toLowerCase();
+//         answer=answer.substring(1);
+        
+        
+//         char a= s.charAt(0);
+//         a=Character.toUpperCase(a);
+        
+//         answer=a+answer;
+//         return answer;
+//         //공백은 다른 단어로 판단하여 대소문자 변환
+        
+//     }
+// }
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        
-        answer=s.toLowerCase();
-        answer=answer.substring(1);
-        
-        
-        char a= s.charAt(0);
-        a=Character.toUpperCase(a);
-        
-        answer=a+answer;
-        return answer;
-        //공백은 다른 단어로 판단하여 대소문자 변환
-        
+          String answer = "";
+          String[] sp = s.toLowerCase().split("");
+          boolean flag = true;
+  
+          for(String ss : sp) {
+              answer += flag ? ss.toUpperCase() : ss;
+              flag = ss.equals(" ") ? true : false;
+          }
+  
+          return answer;
     }
-}
+  }
