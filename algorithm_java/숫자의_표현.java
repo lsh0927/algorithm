f18fe6bf2db0b827c84c701e3201f2b1451c232e
lsh0927 <lsheon0927@naver.com>
2023-03-27 11:09:45 +0900

    약수

--- algorithm_java/숫자의_표현.java
+++ algorithm_java/숫자의_표현.java
@@ -2,15 +2,29 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-       for (int i = 1; i <= n; ++i) {
-        if (n % i == 0) {
-            if(i>=(n+1)/(i*2)&&(n/i)%2!=0)
-            {
-                answer++;
-            }
-        }
-    }
+       
+     for (int i = 1; i <= n; i += 2) 
+       if (n % i == 0) 
+           answer++;
+
         
-        return answer+1;
+        return answer;
     }
 }
+//진짜 쉬운 문제였는데,,,,, 왜 이리 복잡하게 생각했을까 ㅠㅠ
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+        
+//        for (int i = 1; i <= n; ++i) {
+//         if (n % i == 0) {
+//             if(i>=(n+1)/(i*2)&&(n/i)%2!=0)
+//             {
+//                 answer++;
+//             }
+//         }
+//     }
+        
+//         return answer+1;
+//     }
+// }
