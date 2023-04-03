8001b1b6f12c1c8756edb8e5173ef137d0f35a33
lsh0927 <lsheon0927@naver.com>
2023-04-03 15:06:48 +0900

    deleteCharAt

--- algorithm_java/이진변환_반복하기.java
+++ algorithm_java/이진변환_반복하기.java
@@ -26,3 +26,21 @@ class Solution {
         return answer;
     }
 }
+/*
+ * class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int temp;
+        while( !s.equals("1") ) {
+            answer[1] += s.length();
+            s = s.replaceAll("0", "");
+            temp = s.length();
+            s = Integer.toBinaryString(temp);
+            //System.out.println("s : " + s ); 
+            answer[0]++;
+            answer[1] -= temp;
+        }
+        return answer;  
+    }
+}
+ */
