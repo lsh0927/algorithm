098f13e66429f4a1b2c4adb5727a7e8a501fb04f
lsh0927 <lsheon0927@naver.com>
2023-04-12 15:14:35 +0900

    ee

--- algorithm_java/주차_요금_계산.java
+++ algorithm_java/주차_요금_계산.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] solution(int[] fees, String[] records) {
         int[] answer = {};
 
-        
+        answer++;
 
         return answer;
     }
     public int fee(int )
     {
-        
+        ///
     }   
     public 
 }
