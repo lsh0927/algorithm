2ea23295d0053ee0e028be238f449c9a88f433f6
lsh0927 <lsheon0927@naver.com>
2023-05-18 11:33:13 +0900

    시간복잡도

--- algorithm_java/뒷큰수_찾기.java
+++ algorithm_java/뒷큰수_찾기.java
@@ -28,3 +28,60 @@ class Solution {
         return -1;
     }
 }
+//2번째-> 함수 구분 없이 한번에 작성, but 같은 구간에서 시간 초과
+/*
+ * class Solution {
+
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        
+        //자기보다 뒤에 있으면서, 자기보다 큰 가장 가까운 수를 뒷큰수라 칭함
+        //모든 원소에 대한 뒷 큰수들을 차례로 담은 배열을 return
+        //뒷 큰수가 존재하지 않을 시 -1을 담음
+        
+        for(int a=0;a<numbers.length-1;a++)
+        {
+            int count=0;
+            for(int b=a+1;b<numbers.length;b++)
+            {
+                if(numbers[a]<numbers[b])
+                {
+                    answer[a]=numbers[b];
+                    count++;
+                    break;
+                }   
+            }
+            if(count==0)
+                answer[a]=-1;
+        }
+        answer[numbers.length-1]=-1;
+        return answer;
+    }
+   
+}
+ */
+
+
+
+
+//3번째 시도, 처음부터 -1로 초기화 및 뒷큰수 발견시 바로 바꾸고 break해도 같은 곳에서 시간 초과
+ /*class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for (int a = 0; a < numbers.length - 1; a++) {
+            answer[a] = -1; // 초기값 -1로 설정
+            
+            for (int b = a + 1; b < numbers.length; b++) {
+                if (numbers[a] < numbers[b]) {
+                    answer[a] = numbers[b];
+                    break; // 뒷 큰 수를 찾으면 반복문 종료
+                }
+            }
+        }
+        answer[numbers.length-1]=-1;
+        return answer;
+    }
+}
+ */
