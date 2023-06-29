a7f76e69f85e753385283095b3500a2f85880ac8
lsh0927 <lsheon0927@naver.com>
2023-06-29 14:18:54 +0900

    StringBuilder

---
+++ algorithm_java/문자열병합.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        
+        int length1 = word1.length();
+        int length2 = word2.length();
+        int maxLength = Math.max(length1, length2);
+        
+        for (int i = 0; i < maxLength; i++) {
+            if (i < length1) {
+                result.append(word1.charAt(i));
+            }
+            
+            if (i < length2) {
+                result.append(word2.charAt(i));
+            }
+        }
+        
+        return result.toString();
+    }
+}
