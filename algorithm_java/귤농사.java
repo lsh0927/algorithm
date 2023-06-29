ef6693c86b7b2e0b40630a2fd21fff051713e01c
lsh0927 <lsheon0927@naver.com>
2023-06-29 15:42:08 +0900

    for +list

---
+++ algorithm_java/귤농사.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        HashMap<Integer,Integer> map =new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2)-map.get(o1));
+
+        for(Integer key:list){
+            k -=map.get(key);
+            answer++;
+            if(k<=0){
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
