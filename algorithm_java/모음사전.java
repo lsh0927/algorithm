f3f5190256c2493362ed174099afcf54e685152b
lsh0927 <lsheon0927@naver.com>
2023-06-30 11:01:48 +0900

    dfs

--- algorithm_java/모음사전.java
+++ algorithm_java/모음사전.java
@@ -34,3 +34,32 @@ class Solution {
         }
     }
 }
+
+/*
+
+import java.lang.Math;
+
+class Solution {
+    public int solution(String word) {
+        int answer = word.length();
+        int pos;
+        for(pos = 0; pos < word.length(); pos ++){
+            char c = word.charAt(pos);
+            if(c=='A') continue;
+            int temp = 0;
+            for(int i = 0; i <= 4-pos; i++){
+                temp += Math.pow(5,i);
+            }
+            answer += temp*val(c);
+        }
+        return answer;
+    }
+
+    public int val(char c){
+        if(c=='E') return 1;
+        else if(c=='I') return 2;
+        else if(c=='O') return 3;
+        else return 4;
+    }
+}
+ */
