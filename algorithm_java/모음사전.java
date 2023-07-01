31d64b7294b722dd72054f496b9f27ad30169cac
lsh0927 <lsheon0927@naver.com>
2023-07-01 10:37:45 +0900

    dfs,math

--- algorithm_java/모음사전.java
+++ algorithm_java/모음사전.java
@@ -63,3 +63,13 @@ class Solution {
     }
 }
  */
+
+ /*
+  * class Solution {
+    public int solution(String word) {
+        int answer = 0, per = 3905;
+        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+        return answer;
+    }
+}
+  */
