37d56bdec4fec4cdacbc95081a8cdd9a908b99bf
lsh0927 <lsheon0927@naver.com>
2023-07-06 11:46:08 +0900

    재귀? 반복?

---
+++ algorithm_java/124나라의_숫자.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        int a=0;
+        int cnt=3;
+        
+        while(a<n)
+        {
+            a+=cnt;
+            sb.append(4);
+            if(a>=n)
+                break;
+            cnt*=3;          
+        }
+        //n=12라면 a=12, sb=44
+        //n=13이라면 a=39 sb=444
+        
+        //1번의 경우 a-n=0;-> sb=44
+        //2번의 경우 a-n=26 -> sb의 444~411->9개 out 244~211 9개 out 144~112까지 8개 out-> answer=111
+        
+        //그럼 a-n을 compare라고 하고, 여기서 숫자를 빼내는것을 어떻게 구현?
+        // compare가 1이면 마지막 4를 2로 바꾸고, 2면 1로 바꾸고, 3이면 마지막에서 두번째 4를 2로, 4면 두번째와 마지막을 2로,,,,,
+        //진자 복잡하네 이거 아닌거 같은데..
+        
+        
+        
+        return answer;
+    }
+}
