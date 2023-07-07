d0a461356b9adf2d2601ecf345f1c1aa8859421b
lsh0927 <lsheon0927@naver.com>
2023-07-07 13:30:08 +0900

    수학적접근

--- algorithm_java/124나라의_숫자.java
+++ algorithm_java/124나라의_숫자.java
@@ -30,3 +30,24 @@ class Solution {
         return answer;
     }
 }
+
+// class Solution {
+//     public String solution(int n) {
+//         StringBuilder answer = new StringBuilder();
+
+//         while (n > 0) {
+//             int num = n%3;
+//             if (num == 0) {
+//                 answer.insert(0, "4"); 
+//                 n = n/3 - 1;
+//             }
+//             else {
+//                 answer.insert(0, Integer.toString(num));
+//                 n = n/3;
+//             }
+
+//         }
+
+//         return answer.toString();
+//     }
+// }
