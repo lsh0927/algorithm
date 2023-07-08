e08eca78e0459b4c9104f07014d5911797e0c30a
lsh0927 <lsheon0927@naver.com>
2023-07-08 11:08:11 +0900

    배열 비교 &hashmap

--- algorithm_java/할인_행사.java
+++ algorithm_java/할인_행사.java
@@ -17,3 +17,34 @@ class Solution {
      return answer;
  }
 }
+
+/*
+ * import java.util.*;
+
+class Solution {
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            int left = 0;
+            int right = 9;
+            Map<String, Integer> map = new HashMap<>();
+            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
+            while(right < discount.length){
+                if(check(map,want,number)) answer++;
+
+                right++;
+                if(right < discount.length)
+                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
+                map.put(discount[left],map.get(discount[left])-1);
+                left++;
+            }
+
+            return answer;
+        }
+        private boolean check(Map<String,Integer> map,String[] want,int[] number){
+            for(int i =0;i<want.length;i++){
+                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            }
+            return true;
+        }
+    }
+ */
