206d2b01e9231a9ef26744647fe0a46784d69c4d
lsh0927 <lsheon0927@naver.com>
2023-07-11 13:31:23 +0900

    StringBuilder

--- algorithm_java/이상한_문자열_만들기.java
+++ algorithm_java/이상한_문자열_만들기.java
@@ -32,3 +32,31 @@ class Solution {
         return answer;
     }
 }
+/*
+ * class Solution {
+    public String solution(String s) {
+      //문자열을 이용한 풀이
+        StringBuilder builder= new StringBuilder();
+        boolean toUpper=true;
+        
+        for(char c: s.toCharArray())
+        {
+            if(!Character.isAlphabetic(c)){
+                builder.append(c);
+                toUpper=true;
+            }
+            else{
+                if(toUpper)
+                {
+                    builder.append(Character.toUpperCase(c));
+                }
+                else{
+                    builder.append(Character.toLowerCase(c));
+                }
+                toUpper=!toUpper;
+            }
+        }
+        return builder.toString();
+    }
+}
+ */
