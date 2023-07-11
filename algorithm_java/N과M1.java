bc9fe66dbe5afb76d6e88b70fbebf938e3bd6dfd
lsh0927 <lsheon0927@naver.com>
2023-07-11 14:13:34 +0900

    dfs

---
+++ algorithm_java/N과M1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        arr = new int[M];
+        visit= new boolean[N];
+        dfs(N,M,0);
+    }
+    public static void dfs(int N, int M, int depth){
+        if(depth==M){
+            for(int val: arr){
+                System.out.print(val+ " ");
+            }
+            System.out.println();
+            return;
+        }
+        for( int i=0;i<N;i++){
+            if(!visit[i]){
+                visit[i]=true;
+                arr[depth]=i+1;
+                dfs(N,M,depth+1);
+                visit[i]=false;
+            }
+        }
+    }
+}
