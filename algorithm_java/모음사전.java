49f7aaa3e7dee9efc797a8d4c491d5822ac05f26
lsh0927 <lsheon0927@naver.com>
2023-07-14 13:54:17 +0900

    재귀

--- algorithm_java/모음사전.java
+++ algorithm_java/모음사전.java
@@ -73,3 +73,22 @@ class Solution {
     }
 }
   */
+
+  /*
+   * import java.util.ArrayList;
+import java.util.List;
+public class Solution {
+    private static final char[] CHARS = "AEIOU".toCharArray();
+    private void generate(String word, List<String> words) {
+        words.add(word);
+        if (word.length() == 5) return; for (char c : CHARS) {
+            generate(word + c, words); 
+        }
+    }
+    public int solution(String word) {
+    List<String> words = new ArrayList<>(); 
+    generate("", words);
+    return words.indexOf(word);
+    }
+}
+   */
