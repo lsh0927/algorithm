aa65007e4ab0fb949da7d28ed776c096205f407d
lsh0927 <lsheon0927@naver.com>
2023-08-10 11:28:44 +0900

    큐

---
+++ algorithm_java/다리를_지나는_트럭.java
@@ -0,0 +1,36 @@
+class Solution {
+            public int solution(int bridge_length, int weight, int[] truck_weights) {
+                int bridgeWeight=0;
+                Queue<Integer>bridge= new LinkedList<>();
+
+                for(int i=0;i<bridge_length;i++)
+                {
+                    bridge.add(0);
+                }
+
+                int time=0;
+                int truckIndex=0;
+
+                while (truckIndex<truck_weights.length){
+                    bridgeWeight-=bridge.poll();
+                    int truckWeight=truck_weights[truckIndex];
+                    if(bridgeWeight+truckWeight<=weight)
+
+                    {
+                        bridge.add(truckWeight);
+                        bridgeWeight+=truckWeight;
+                        truckIndex++;
+                    }
+                    else{
+                        bridge.add(0);
+                    }
+                    time++;
+                }
+                while(bridgeWeight>0){
+                    bridgeWeight-=bridge.poll();
+                    time++;
+                }
+
+                return time;
+            }
+        }
