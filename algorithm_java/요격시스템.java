f7d044bf3e65f750c00d74bc2630092c9c9a49f5
lsheon0927@naver.com <lsheon0927@naver.com>
2023-09-07 20:12:13 +0900

    범위 나누기

---
+++ algorithm_java/요격시스템.java
@@ -0,0 +1,18 @@
+class Solution {
+            public int solution(int[][] targets) {
+                Arrays.sort(targets,(a,b)->a[0]-b[0]);
+                int cnt=0;
+                int last=-1;
+                for(int i=0;i<targets.length;i++){
+                    int[] missile=targets[i];
+                    if(missile[0]>last){
+                        cnt++;
+                        last=missile[1]-1;
+                    }
+                    else if(missile[1]-1>last){
+                        last=missile[1]-1;
+                    }
+                }
+                return cnt;
+            }
+        }
