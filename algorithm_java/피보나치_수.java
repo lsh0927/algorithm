f8cc15ec67c0a702d0623d998c77d02f6942de2d
lsheon0927@naver.com <lsheon0927@naver.com>
2023-09-18 09:44:59 +0900

    dp+시간복잡도

--- algorithm_java/피보나치_수.java
+++ algorithm_java/피보나치_수.java
@@ -12,19 +12,41 @@
 // }
 
 
-class Solution { //시간을 아끼는 방법 1
+// class Solution { //시간을 아끼는 방법 1
 
-    public int solution(int n) {
-        int answer = 0;
-        long[] pib=new long[n+1];
-        pib[0]=0L;
-        pib[1]=1L;
-        int times=1;
-        for(int i=2; i<=n; i++){
-            pib[i]=(pib[i-1]+pib[i-2])%1234567L;
+//     public int solution(int n) {
+//         int answer = 0;
+//         long[] pib=new long[n+1];
+//         pib[0]=0L;
+//         pib[1]=1L;
+//         int times=1;
+//         for(int i=2; i<=n; i++){
+//             pib[i]=(pib[i-1]+pib[i-2])%1234567L;
+//         }
+//         //answer=(int)(pib[n]%1234567L);
+//         answer=(int)(pib[n]);
+//         return answer;
+//     }
+// }
+import java.util.*;
+
+class Solution {
+            //배열 선언, 종료조건 설정
+            private final int[] dp= new int[100001];
+            private int fibo(int n){
+                if(dp[n]!= -1)
+                    return dp[n];
+                if(n==0 || n==1) 
+                    return n;
+                
+                return dp[n]=(fibo(n-1)+fibo(n-2)) %1234567;
+            }
+            
+            public int solution(int n) {
+                Arrays.fill(dp,-1);
+                // for(int i=0;i<=n;i++)
+                //     fibo(i);
+                    
+                return fibo(n);
+            }
         }
-        //answer=(int)(pib[n]%1234567L);
-        answer=(int)(pib[n]);
-        return answer;
-    }
-}
