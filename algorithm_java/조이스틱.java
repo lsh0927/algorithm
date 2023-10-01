b35141cd0ee76b16bd266fe1197d46b3544af2d1
lsheon0927@naver.com <lsheon0927@naver.com>
2023-10-01 15:35:04 +0900

    two pointer

--- algorithm_java/조이스틱.java
+++ algorithm_java/조이스틱.java
@@ -1,43 +1,42 @@
-class Solution {
+ class Solution {
             public int solution(String name) {
-                char[] nameArr=name.toCharArray();
-                int cost=0;
-                for(int i=0;i<name.length();i++){
-                    cost+=compareAlphabet(name.charAt(i));
+                int cost = 0;
+
+                for(int i = 0; i<name.length(); i++){
+                    int diff = name.charAt(i) - 'A';
+                    if(diff > 13)
+                        diff = 26 - diff;
+                    cost += diff;
                 }
-                int front =0;
-                int end=front;
-                while (end<name.length() && name.charAt(end)=='A')
+
+                int front = 0;
+                int end = front;
+                while(end < name.length() && name.charAt(end) == 'A')
                     end++;
-                int minVerCost=0;
-                while (end<name.length()) {
+
+                int minVerCost = 0;
+                while(end < name.length()){
                     int backDistance = name.length() - end;
                     int verCost = front + backDistance;
-                    if (front < backDistance)
+                    if(front < backDistance)
                         verCost += front;
                     else
                         verCost += backDistance;
-                    if (minVerCost == 0)
+                    if(minVerCost == 0)
                         minVerCost = verCost;
-                    else if (minVerCost > verCost) {
+                    else if(minVerCost > verCost)
                         minVerCost = verCost;
-                    }
 
                     front = end;
                     end++;
-                    while (end < name.length() && name.charAt(end) == 'A')
+                    while(end < name.length() && name.charAt(end) == 'A')
                         end++;
                 }
-                if(front<minVerCost)
-                    minVerCost=front;
+                if(front < minVerCost)
+                    minVerCost = front;
 
-                cost+= minVerCost;
+                cost += minVerCost;
+
                 return cost;
-            }
-            public int compareAlphabet(char alp){//현재 커서 위치
-                int count=0;
-                if(alp-'A'>13) count=26-(alp-'a');
-                else count=alp-'a';
-                return count;
             }
         }
