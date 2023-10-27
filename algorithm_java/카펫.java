eda838e499106587625c691dcbb1dee90a548446
lsheon0927@naver.com <lsheon0927@naver.com>
2023-10-27 14:07:29 +0900

    완전탐색

--- algorithm_java/카펫.java
+++ algorithm_java/카펫.java
@@ -1,42 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sum = brown + yellow;
         
-        for (int i = 3; i < sum; i++) {
-            int j = sum / i;
-            
-            if (sum % i == 0 && j >= 3) {
-                int col = Math.max(i, j);
-                int row = Math.min(i, j);
-                int center = (col - 2) * (row - 2);
-                
-                if (center == yellow) {
-                    answer[0] = col;
-                    answer[1] = row;
-                    return answer;
-                }
+        for(int width=3;width<=5000; width++){
+            for (int height=3; height<=width; height++){
+                int boundary=(width+height-2)*2;
+                int center= width*height-boundary;
+                if(brown==boundary && yellow==center)
+                    return new int[] {width,height};
             }
         }
+        int[] answer = {};
         return answer;
     }
+    //주어진 조건을 만족하는 카펫의 가로 길이와 세로 길이를 구해야 함.
+    //바깥 격자의 갯수 -> (width+height-2)*2 개
+    //따라서 안쪽 격자 = (width * height) - (width+height-2) * 2
 }
-
-/*
- * class Solution {
-    public int[] solution(int brown, int yellow) {
-        for(int width=3; width<=5000;width++){
-            for(int height=3; height<=width; height++){
-                int boundary = (width + height - 2) * 2; 
-                int center = width * height - boundary;
-                
-                if (brown == boundary && yellow == center) { 
-                        return new int[] {width, height};
-                }
-            }
-        }
-        return null;
-    }
-
-}
- */
