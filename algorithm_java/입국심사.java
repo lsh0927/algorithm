1f4417eb9f68fe1ff1e32f9a48797454232fac3b
lsheon0927@naver.com <lsheon0927@naver.com>
2023-11-02 11:00:20 +0900

    이진탐색

--- algorithm_java/입국심사.java
+++ algorithm_java/입국심사.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-        class Solution {
+ class Solution {
             public long solution(int n, int[] times) {
-                long start=1;
+                long start = 0;
                 long end=1000000000000000000L;
-                while(end>start){
-                    long t=(start+end)/2;
-                    if(isVaild(t,n,times)){
+                
+                // 조건을 만족하는 값 중 가장 작은 값을 찾는 이진 탐색을 구현해야 함
+                // 범위 표기는 [start,end]가 되고, 이에 따라 end 변수에는 범위의
+                // 실제 최댓값인 10억이 들어감
+                
+                while(end>start)
+                {
+                    long t= (start+end)/2;
+                    //정답 검사 및 범위 좁히기
+                    if(isValid(t,n,times))
                         end=t;
-                    }
-                    else {
-                        start=t+1;
-                    }
+                    else start= t+1;
                 }
+                
                 return start;
             }
-            private boolean isVaild(long t, int n, int[] times)
-            {
+            private boolean isValid(long t, int n, int[] times){
                 long c=0;
-                for(int time: times){
+                for (int time : times) {
                     c+=t/time;
                 }
                 return c>=n;
             }
         }
-
- 
