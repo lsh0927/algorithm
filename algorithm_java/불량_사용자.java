026ec7a726dd64790480c0eb688be3bccce9800d
lsheon0927@naver.com <lsheon0927@naver.com>
2023-11-29 16:09:47 +0900

    stream

--- algorithm_java/불량_사용자.java
+++ algorithm_java/불량_사용자.java
@@ -1,55 +1,39 @@
-import javax.swing.*;
-import java.io.*;
 import java.util.*;
 
-import static java.lang.Math.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        class Solution {
+class Solution {
             public int solution(String[] user_id, String[] banned_id) {
-                String[][] bans = Arrays.stream(banned_id)
-                        .map(banned -> banned.replace('*', '.'))
-                        .map(banned -> Arrays.stream(user_id)
-                                .filter(id -> id.matches(banned))
-                                .toArray(String[]::new))
-                        .toArray(String[][]::new);
 
-                Set<Set<String>> banSet=new HashSet<>();
-                count(0,new HashSet<>(),bans,banSet);
-                return banSet.size();
+               String[][] bans= Arrays.stream(banned_id)
+                                .map(banned->banned.replace('*','.'))
+                                .map(banned->Arrays.stream(user_id)
+                                        .filter(id -> id.matches(banned))
+                                        .toArray(String[]::new))
+                                .toArray(String[][]::new);
+
+                Set<Set<String>> banSet= new HashSet<>();
+               count(0,new HashSet<>(),bans,banSet);
+               return banSet.size();
             }
-            //상태를 이용한 메서드 선언
-            private void count(int index, Set<String> banned,String[][] bans,Set<Set<String>> banSet){
-                if(index==bans.length){
+            private void count(int index,Set<String> banned,
+                               String[][] bans, Set<Set<String>> banSet){
+                //count의 매개변수 중, index와 banned는 상태변수
+                // bans는 불량 사용자 아이디별로 매칭되는 사용자 아이디 정보
+                // banSet은 찾은 조합을 저장할 매개변수
+
+                //종료조건
+                if (index== bans.length){
                     banSet.add(new HashSet<>(banned));
                     return;
                 }
-                for (String id: bans[index]){
-                    if(banned.contains(id))
-                        continue;
-                    count(index+1,banned,bans,banSet);
+
+                //점화식
+                for (String id : bans[index]) {
+                    if (banned.contains(id)) continue;
+                    banned.add(id);
+                    count(index+1, banned,bans,banSet);
                     banned.remove(id);
                 }
             }
         }
-    }
-}
-            //정규 표현식을 이용해 불량 사용자 아이디 목록에 해당하는 사용자 아이디를 구함
-            //조합할 수 있는 경우의 수를 구해야 함.
-
-                /*
-                재귀 정의
-                앞서 구한 bans를 이용해 서로 다른 사용자 아이디의 조합 개수를 세어주어야 함
-                원소를 순서대로 나열하는 개수인 순열과 비슷하게 재귀로 구현되지만,
-                순서가 상관없다는 점을 생각하면 Set를 이용할 수 있음
-
-                상태: 불량 사용자 아이디 중 하나의 사용자 아이디를 선택해야 함
-                따라서 불량 사용자 아이디의 순서대로 재귀를 진행할 수 있으며, 상태에는
-                사용자 아이디를 선택할 불량 사용자 아이디의 인덱스와
-                중복된 사용자  아이디 선택을 방지하는 Set 자료구조인 banned로 구성
-
-                따라서 이 문제의 재귀 상태는 (index, banned)로 표기할 수 있고,
-                이 상태가 나타내는 부분 문제는 banned에 포함된 사용자 아이디를 제외하고
-                불량 사용자 아이디 index 부터 가능한 사용자 아이디의 조합이 됨
-                 */
+        //가능한 경우의 수
+        //일부 문자를 *글자로 가려서 전달하였음
