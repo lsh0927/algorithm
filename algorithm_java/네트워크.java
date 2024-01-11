413fbb2171c887ac47f5b8e345bb09a8fcc3f98a
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-11 18:21:10 +0900

    dfs

--- algorithm_java/네트워크.java
+++ algorithm_java/네트워크.java
@@ -39,3 +39,30 @@
                 return answer;
             }
         }
+
+        import java.util.*;
+
+class Solution {
+    static boolean[] visit;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visit= new boolean[n];
+        
+        for(int i=0;i<n;i++){
+            if(visit[i]==false){
+                answer++;
+                DFS(i,computers,n);
+            }
+        }
+        return answer;
+    }
+    private void DFS(int i, int[][]computers,int n){
+        visit[i]=true;
+        
+        for(int j=0;j<n;j++){
+            if(visit[j]==false && computers[i][j]==1){
+                DFS(j,computers,n);
+            }
+        }
+    }
+}
