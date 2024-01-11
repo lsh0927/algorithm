b9f204e2c623e026097f32d5f0c8943d61de708e
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-11 17:14:20 +0900

    <fix> dfs 구조

--- algorithm_java/타겟넘버.java
+++ algorithm_java/타겟넘버.java
@@ -1,3 +1,52 @@
+// import java.util.*;
+
+// class Solution {
+//     //DFS
+    
+//     public static class State{
+//         int acc;
+//         int step;
+        
+//         public State (int acc, int step){
+//             this.acc=acc;
+//             this.step=step;
+//         }
+//     }
+//     static int count=0;
+//     static int t;
+//     public int solution(int[] numbers, int target) {
+        
+//         State state=new State(0,0);
+        
+//         int len= numbers.length;
+        
+//         dfs(state,len,numbers,target);
+//         //현재 상태는 누적합 0, 인덱스 0;
+        
+        
+//         int answer = count;
+//         return answer;
+//     }
+//     private static void dfs(State state,int len,int[] numbers, int target){
+        
+//         if(state.step==len)
+//         {
+//             if(state.acc==target)
+//             {
+//                 count++;
+//                 return;
+//             }
+//             return;
+//         }
+
+//         state.acc-=numbers[state.step++];
+//         dfs(state,len,numbers,target);
+//         state.acc+=numbers[state.step++];
+        
+//     }
+// }
+
+
 import java.util.*;
 
 class Solution {
@@ -12,36 +61,36 @@ class Solution {
             this.step=step;
         }
     }
-    static int count=0;
+    static int count;
     static int t;
     public int solution(int[] numbers, int target) {
         
         State state=new State(0,0);
+        count=0;
         
         int len= numbers.length;
         
-        dfs(state,len,numbers,target);
+        dfs(state.acc,state.step,len,numbers,target);
         //현재 상태는 누적합 0, 인덱스 0;
         
         
-        int answer = count;
-        return answer;
+        return count;
     }
-    private static void dfs(State state,int len,int[] numbers, int target){
+    
+    private void dfs(int acc, int step,int len,int[] numbers, int target){
         
-        if(state.step==len)
+        if(step==len)
         {
-            if(state.acc==target)
+            if(acc==target)
             {
                 count++;
                 return;
             }
-            return;
+            else 
+                return;
         }
 
-        state.acc-=numbers[state.step++];
-        dfs(state,len,numbers,target);
-        state.acc+=numbers[state.step++];
-        
+        dfs(acc-=numbers[step],step+1,len,numbers,target);
+        dfs(acc+=numbers[step],step+1,len,numbers,target);
     }
 }
