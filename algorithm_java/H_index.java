7915ea482fad9320b4adf942852d3edcef867d53
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-20 13:32:03 +0900

    index

--- algorithm_java/H_index.java
+++ algorithm_java/H_index.java
@@ -1,40 +1,3 @@
-// import java.util.Arrays;
-
-// class Solution {
-//     public int solution(int[] citations) {
-//         int answer = 0; 
-//         Arrays.sort(citations);
-    
-//         int a=citations.length;     
-//         // n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
-        
-//         int count=0,i=0;
-//       while(citations[i]<=a-count)
-//       {
-//            i++;
-//            count++;
-//       }
-//         return a-count;
-//     }
-// }
-// import java.util.*;
-
-// class Solution {
-//     public int solution(int[] citations) {
-//         int answer = 0;
-
-//         Arrays.sort(citations);
-//         for(int i=0; i<citations.length; i++){
-
-//             if(citations[i]>=citations.length-i){
-//                 answer=citations.length-i;
-//                 break;
-//             }
-
-//         }
-//         return answer;
-//     }
-// }
 
 import java.util.Arrays;
 
@@ -57,3 +20,36 @@ class Solution {
         return answer;
     }
 }
+/*
+ * import java.util.Arrays;
+
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int len = citations.length;
+
+        Arrays.sort(citations);
+
+        for(int i = len; i >= 1; i--) {
+            
+            int h = 0;
+            for(int j = 0; j < len; j++) {
+                if(citations[j] >= i) {
+                    h++;
+                }
+            }
+            
+            if(h >= i && len - h <= i) {
+                answer = i;
+                break;
+            }
+            
+        }
+
+        return answer;
+    }
+}
+
+
+ */
