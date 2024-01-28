f8a17935e49be8ee38dced33012c2f805f50666e
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-28 11:35:30 +0900

    dp

--- algorithm_java/2156.java
+++ algorithm_java/2156.java
@@ -15,7 +15,7 @@ class Main {
         dp= new Integer[N+1];
         arr= new int[N+1];
 
-        for (int i=1;i<N;i++){
+        for (int i=1;i<N+1;i++){
             arr[i]=Integer.parseInt(br.readLine());
         }
 
@@ -36,4 +36,16 @@ class Main {
         
         return dp[N];
     }
+    /*
+     * 재귀를 이용한 top-down 방식
+     * 
+     * 
+     * 반복문을 이용한 방식
+     * for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+ 
+		}
+		System.out.println(dp[N]);
+	}
+     */
 }
