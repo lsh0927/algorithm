17668d9647834956b70a12cc3874f2b9a33dfc52
lsheon0927@naver.com <lsheon0927@naver.com>
2024-03-21 11:58:44 +0900

    greedy

--- algorithm_java/12904.java
+++ algorithm_java/12904.java
@@ -1,37 +1,24 @@
 import java.util.*;
 import java.io.*;
-class 12904 {
+ class 12904 {
 
-    static List<String> list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String S= br.readLine();
-        String T= br.readLine();
-        list= new ArrayList<>();
+        StringBuilder s= new StringBuilder(br.readLine());
+        StringBuilder t= new StringBuilder(br.readLine());
 
-        dfs(S,T);
-
-        if (list.contains(T))
+        while (s.length()<t.length()){
+            if (t.charAt(t.length()-1)=='A'){
+                t.deleteCharAt(t.length()-1);
+            } else if (t.charAt(t.length()-1)=='B') {
+                t.deleteCharAt(t.length()-1);
+                t.reverse();
+            }
+        }
+        if (t.toString().equals(s.toString()))
             System.out.println(1);
+
         else System.out.println(0);
-    }
-    public static void dfs(String S, String T){
-        if (S.length()==T.length()){
-            list.add(S);
-            return;
-        }
-        String s1= S+"A";
-
-        char arr[]= S.toCharArray();
-        String s2="";
-        for (int i= arr.length-1;i>=0;i--){
-            s2+=arr[i];
-        }
-        s2+="B";
-                //문자열을 거꾸로 뒤집는 방법
-        dfs(s1,T);
-        dfs(s2,T);
-
     }
 }
