b9a3e3a250ada1c8959937a70782921e75e9db47
lsheon0927@naver.com <lsheon0927@naver.com>
2024-07-19 12:38:28 +0900

    recur order

--- algorithm_java/1463.java
+++ algorithm_java/1463.java
@@ -1,33 +1,53 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
+
     static Integer[] dp;
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N= sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        dp= new Integer[N+1];
-        dp[0]=dp[1]=0;
-        System.out.println(recur(N));
+        int n= Integer.parseInt(br.readLine());
+
+        dp= new Integer[n+1];
+
+        dp[0]=0;
+        dp[1]=0;
+//        dp[2]=1;
+//        dp[3]=1;
+
+
+
+        System.out.println(find(n));
     }
+    static int find(int n){
 
-    static int recur(int N){
-        if(dp[N]==null)
-        {
-            if(N%6==0){
-                dp[N]=Math.min(recur(N-1),Math.min(recur(N / 3), recur(N / 2))) + 1;
-            } else if (N%3==0){
-                dp[N]=Math.min(recur(N/3),recur(N-1))+1;
+
+        if (dp[n]==null){
+
+
+
+            if (n%6==0){
+                dp[n]=Math.min(find(n-1),Math.min(find(n/3),find(n/2)))+1;
+
+
+            } else if (n%3==0) {
+                dp[n]=Math.min(find(n/3),find(n-1))+1;
             }
-            else if (N%2==0){
-                dp[N]=Math.min(recur(N/2),recur(N-1))+1;
+
+            else if (n%2==0) {
+                dp[n]=Math.min(find(n/2),find(n-1))+1;
             }
-            else{
-                dp[N]=recur(N-1)+1;
+            else {
+                dp[n]=find(n-1)+1;
             }
         }
-        return dp[N];
+
+
+        return dp[n];
+
     }
 }
